6202acc02beb4500d78ac969b162ee3d06620881
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-11-15 15:45:19 -0700

    Develop (#3)
    * Generates .mcpack file
    
    * Added cloudflare adapter
    
    * Forgot to add package
    
    * Giving it a try
    
    * Remove
    
    * Cleanned up and added fields for title and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import {buildManifest, generateZip} from "$lib/builder"
     import manifestJson from "../lib/assets/manifest.json"
     let data: any
-
+    
     let files
 
     const handleFileChange = (event) => {
@@ -11,17 +11,21 @@
     
     function handleSubmit(event) {
         event.preventDefault()
-
-        console.log(event.target.file[0])
-        generateZip(buildManifest(manifestJson), files)
+        generateZip(buildManifest(manifestJson, event.target.title.value, event.target.description.value), files)
     }
   </script>
 
-<div class="flex-1 flex items-center justify-center">
+<div class="flex-1 flex items-center justify-center text-white">
     <div class="bg-gray-800 items-center p-10 rounded shadow">
-    <div>Upload your .mcstructure files and a Minecraft Structure Pack will be generated for you. <br/> Then you will be able to import the pack into your game.</div>
+    <h1 class="text-3xl text-center">Minecraft Bedrock Structure Pack Generator</h1> <br> <br>
+    <div>Upload your .mcstructure files and a Minecraft Structure Pack will be generated for you. <br> 
+      Then you will be able to import the pack into your game.</div> <br>
       <form enctype="multipart/form-data" on:submit|preventDefault={handleSubmit}>
         <div>
+          <label for="title">Pack Title</label><br>
+          <input class="text-black" type="text" id="title" name="title" value="Minecraft Bedrock Structure Pack"> <br>
+          <label for="description">Pack Description</label><br>
+          <input class="text-black" type="text" id="description" name="description" value="Custom Minecraft Bedrock Structures"><br><br>
           <label for="file">Upload your file</label>
           <input
             on:change={handleFileChange}
@@ -34,7 +38,6 @@
             bind:value={data}
           />
         </div>
-      
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-6 rounded-full mt-4" type="submit">Submit</button>
       </form>
     </div>
