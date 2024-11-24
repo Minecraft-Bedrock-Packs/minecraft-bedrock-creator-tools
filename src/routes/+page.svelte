75954f76d192a2ab5168ad8af8951ca9b57f0f85
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-11-24 11:02:00 -0700

    Moved tools to their own folder. Redid the tailwindcss to be cleaner and better designed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,21 @@
-<script lang="ts">
-    import {buildManifest, generateZip} from "$lib/builder"
-    import manifestJson from "../lib/assets/manifest.json"
-    let data: any
-    
-    let files
+<svelte:head>
+	<title>Minecraft Bedrock Creator Tools</title>
+</svelte:head>
 
-    const handleFileChange = (event) => {
-      files = event.target.files
-    };
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        generateZip(buildManifest(manifestJson, event.target.title.value, event.target.description.value), files)
-    }
-  </script>
-
-<div class="flex-1 flex items-center justify-center text-white">
-    <div class="bg-gray-800 items-center p-10 rounded shadow">
-    <h1 class="text-3xl text-center">Minecraft Bedrock Structure Pack Generator</h1> <br> <br>
-    <div>Upload your .mcstructure files and a Minecraft Structure Pack will be generated for you. <br> 
-      Then you will be able to import the pack into your game.</div> <br>
-      <form enctype="multipart/form-data" on:submit|preventDefault={handleSubmit}>
-        <div>
-          <label for="title">Pack Title</label><br>
-          <input class="text-black" type="text" id="title" name="title" value="Minecraft Bedrock Structure Pack"> <br>
-          <label for="description">Pack Description</label><br>
-          <input class="text-black" type="text" id="description" name="description" value="Custom Minecraft Bedrock Structures"><br><br>
-          <label for="file">Upload your file</label>
-          <input
-            on:change={handleFileChange}
-            type="file"
-            id="file"
-            name="fileToUpload"
-            multiple={true}
-            accept=".mcstructure"
-            required
-            bind:value={data}
-          />
-        </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-6 rounded-full mt-4" type="submit">Submit</button>
-      </form>
-    </div>
+<div class="flex min-h-screen flex-col justify-between p-8">
+    <main class="flex flex-grow flex-col items-center justify-center">
+        <div>I built these tools because I got annoyed when creating Minecraft Bedrock addons.</div>
+        <div class="mt-4"></div>
+        <a href="/structureblock" class="text-blue-500 hover:underline">Structure Block Pack Generator</a>
+    </main>
+    <footer class="bg-black mt-8 text-center text-sm text-gray-500">
+        <a
+          href="https://github.com/Minecraft-Bedrock-Packs/minecraft-bedrock-structure-block-pack-generator-website/tree/main"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:underline"
+        >
+          View on GitHub
+        </a>
+      </footer>
 </div>
-
