17b9f3bd6dc7f50ba1b8a831ae4bd02fdc69803f
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2025-02-14 10:56:59 -0700

    Changing to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-node';
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown
 			out: 'build',
 			precompress: true,
 			envPrefix: ''
