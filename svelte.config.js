0d2f568b3c0d548392ffd011174c180fe0cf5918
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2025-02-14 10:45:38 -0700

    Changing to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: true,
+			envPrefix: ''
+		})
+	}};
